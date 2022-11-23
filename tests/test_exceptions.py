a1bac201c454c7881df74382b7938dc274bcf272
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2022-11-23 15:05:54 +1300

    Add generate access token endpoint
    Update tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,6 +3,8 @@ The Exceptions test module.
 
 This module contains tests for Exceptions.
 """
+from typing import Optional
+
 import pytest
 
 from himon.exceptions import AuthenticationError, ServiceError
@@ -11,7 +13,9 @@ from himon.league_of_comic_geeks import LeagueofComicGeeks
 
 def test_unauthorized():
     """Test generating an AuthenticationError."""
-    session = LeagueofComicGeeks(api_key="Invalid", client_id="Invalid", cache=None)
+    session = LeagueofComicGeeks(
+        client_id="Invalid", client_secret="Invalid", access_token=None, cache=None
+    )
     with pytest.raises(AuthenticationError):
         session.comic(comic_id=1)
 
@@ -19,14 +23,15 @@ def test_unauthorized():
 def test_not_found(session: LeagueofComicGeeks):
     """Test a 404 Not Found raises a ServiceError."""
     with pytest.raises(ServiceError):
-        session._get_request(endpoint="/invalid")
+        session._json_get_request(endpoint="/invalid")
 
 
-def test_timeout(league_of_comic_geeks_api_key: str, league_of_comic_geeks_client_id: str):
+def test_timeout(client_id: str, client_secret: str, access_token: Optional[str]):
     """Test a TimeoutError for slow responses."""
     session = LeagueofComicGeeks(
-        api_key=league_of_comic_geeks_api_key,
-        client_id=league_of_comic_geeks_client_id,
+        client_id=client_id,
+        client_secret=client_secret,
+        access_token=access_token,
         timeout=0.1,
         cache=None,
     )
