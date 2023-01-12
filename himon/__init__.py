7b04794c1671c78524c2056299cb9b2535362291
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2023-01-12 16:43:14 +1300

    Add XDG Support
    Removed tox

--- himon/__init__.py
+++ himon/__init__.py
@@ -1,17 +1,19 @@
 """himon package entry file."""
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __all__ = ["__version__", "get_cache_root"]
 
+import os
 from pathlib import Path
 
 
 def get_cache_root() -> Path:
     """
-    Create and return the path to the cache for Himon.
+    Create and return the path to the cache for Himon, supports XDG_CACHE_HOME env.
 
     Returns:
-        The path to the Himon cache
+        The path to the Himon cache folder.
     """
-    folder = Path.home() / ".cache" / "himon"
+    cache_home = os.getenv("XDG_CACHE_HOME", default=str(Path.home() / ".cache"))
+    folder = Path(cache_home).resolve() / "simyan"
     folder.mkdir(parents=True, exist_ok=True)
     return folder
